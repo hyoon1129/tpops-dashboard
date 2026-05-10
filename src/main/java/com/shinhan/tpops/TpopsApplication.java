a230d5db5fd1ad2a0251ff5b0db77122057a649c
hyoon1129 <hyoon1129@naver.com>
2026-05-10 13:01:23 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/shinhan/tpops/TpopsApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.tpops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shinhan/tpops/TpopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.tpops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
